94d832346c42d3ac2b267c4eb9d87326b9ed69a0
kgandhi09 <kushal009gandhi@gmail.com>
2019-12-04 03:05:34 -0500

    dijsktra's working!

---
+++ Dijkstra's Algorithm/src/dijsktra.cpp
@@ -0,0 +1,29 @@
+/*
+ * dijsktra.cpp
+ *
+ *  Created on: Dec 3, 2019
+ *      Author: kushal
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <iostream>
+int main(int argc, char** argv){
+
+	return 0;
+}
+
+int fieldArr[6][6];
+int rowVal = 0;
+int rowCol = 0;
+
+//for(int i = 0; i < 6; i++){
+//	fieldArr[0][i] = rowVal;
+//	rowVal++;
+//}
+//
+//for(int i = 0; i < 6; i++){
+//	fieldArr[i][0] = colVal;
+//	colVal++;
+//}
